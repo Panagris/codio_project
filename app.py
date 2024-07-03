603d2194a439d6db6857bb2dce1c1b3e36a9aaef
Chiagozie Okoye <c.i.okoye@wustl.edu>
2024-07-03 09:02:40 -0700

    Set up venv

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Python Interpreter: 3.12.4 (.venv)
 from flask import Flask, render_template
 import os
 
@@ -12,5 +13,10 @@ def home_page():
                            text='This is the welcome page')
 
 
+'''
+This is an example of the Flask framework "calling" the code, rather than the
+code calling a function. The framework calls the programmer-defined functions
+such as home_page().
+'''
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
