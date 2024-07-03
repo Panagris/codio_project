00e546e38cd8419e2f80b5eead089ba313ee00bb
Chiagozie Okoye <c.i.okoye@wustl.edu>
2024-07-03 11:31:27 -0700

    Trying resolve 500 for /register

--- main.py
+++ main.py
@@ -8,21 +8,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
-app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+FLASK_SECRET_KEY = '26e5210299d1e99574c25f6657c90fa2'
+app.config['SECRET_KEY'] = FLASK_SECRET_KEY
+# os.getenv('FLASK_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(20), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(60), nullable=False)
 
-  def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
 
 with app.app_context():
-  db.create_all()
+    db.create_all()
 
 
 '''
@@ -42,7 +46,8 @@ def hello_world():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():  # checks if entries are valid
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        user = User(username=form.username.data, email=form.email.data,
+                    password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
