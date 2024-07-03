d7d129a5a267ad8c96bc251c9f28e12b42668c97
Chiagozie Okoye <c.i.okoye@wustl.edu>
2024-07-03 09:57:25 -0700

    Finished with Codio basic Flask app website

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 # Python Interpreter: 3.12.4 (.venv)
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
 
 app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
-
-
-@app.route('/')
-@app.route("/home")
-def home_page():
-    return render_template('home.html', subtitle='Welcome',
-                           text='This is the welcome page')
-
 
 '''
 This is an example of the Flask framework "calling" the code, rather than the
 code calling a function. The framework calls the programmer-defined functions
 such as home_page().
 '''
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', subtitle='Home Page',
+                           text='This is the home page')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():  # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))  # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8000)
